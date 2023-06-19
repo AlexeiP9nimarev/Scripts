7dcc6ba88d975674c49030fde1840b4f45b4ca49
AlexeiP9nimarev <96121557+AlexeiP9nimarev@users.noreply.github.com>
2023-06-19 21:38:32 +0500

    Add files via upload

---
+++ Example3.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example3 : MonoBehaviour
+{
+    List<string> Array;
+    Rigidbody obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // obj = GetComponent<Rigidbody>();
+        // Array.Add("hello");
+        // Array.Add("world");
+        // foreach (string chr in Array){
+        //     print(chr);
+        // }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     if (obj.isKinematic)
+        //     {
+        //         obj.isKinematic = false;
+        //     }else {
+        //         obj.isKinematic = true;
+        //     }
+                
+        // }
+
+    }
+}
